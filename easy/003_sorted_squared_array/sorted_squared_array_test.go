f5c69bb346c4372ed132301cb7b648a3053934c8
Alim Nastaev <32502172+alimnastaev@users.noreply.github.com>
2023-06-18 21:43:04 -0400

    Sorted Squared Array tests refaactored

--- easy/003_sorted_squared_array/sorted_squared_array_test.go
+++ easy/003_sorted_squared_array/sorted_squared_array_test.go
@@ -6,20 +6,39 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCase1(t *testing.T) {
-	array := []int{2, 3, 5, 6, 8, 9}
-	sequence := []int{4, 9, 25, 36, 64, 81}
-	require.Equal(t, SortedSquaredArray2(array), sequence)
-}
+func TestSortedSquaredArray(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []int
+		want  []int
+	}{
+		{
+			name:  "1",
+			input: []int{2, 3, 5, 6, 8, 9},
+			want:  []int{4, 9, 25, 36, 64, 81},
+		},
+		{
+			name:  "2",
+			input: []int{-4, -1},
+			want:  []int{1, 16},
+		},
+		{
+			name:  "3",
+			input: []int{-7, -3, 1, 9, 22, 30},
+			want:  []int{1, 9, 49, 81, 484, 900},
+		},
+	}
 
-func TestCase2(t *testing.T) {
-	array := []int{-4, -1}
-	sequence := []int{1, 16}
-	require.Equal(t, SortedSquaredArray2(array), sequence)
-}
+	got := []int{}
 
-func TestCase3(t *testing.T) {
-	expected := []int{1, 9, 49, 81, 484, 900}
-	actual := SortedSquaredArray2([]int{-7, -3, 1, 9, 22, 30})
-	require.Equal(t, expected, actual)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got = SortedSquaredArray1(tt.input)
+			require.Equal(t, got, tt.want)
+
+			got = SortedSquaredArray2(tt.input)
+			require.Equal(t, got, tt.want)
+		})
+	}
 }
