e6655efdb4022d50f362d075d44ace2e82c1f1cd
Alim Nastaev <32502172+alimnastaev@users.noreply.github.com>
2023-07-30 18:13:28 -0400

    24 polished + docs + more tests

--- easy/024_generate_document/generate_document_test.go
+++ easy/024_generate_document/generate_document_test.go
@@ -9,8 +9,19 @@ import (
 func Test_RunLength(t *testing.T) {
 	characters := "Bste!hetsi ogEAxpelrt x "
 	document := "AlgoExpert is the Best!"
-
 	expected := true
 	actual := GenerateDocument(characters, document)
+	require.Equal(t, expected, actual)
+
+	characters = "a hsgalhsa sanbjksbdkjba kjx"
+	document = ""
+	expected = true
+	actual = GenerateDocument(characters, document)
+	require.Equal(t, expected, actual)
+
+	characters = "aheaolabbhb"
+	document = "hello"
+	expected = false
+	actual = GenerateDocument(characters, document)
 	require.Equal(t, expected, actual)
 }
